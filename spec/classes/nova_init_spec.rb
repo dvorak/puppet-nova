f716f717ef8394a61fae4c8aea61475858f41cb0
Matt Fischer <matt@mattfischer.com>
2014-06-19 10:33:29 -0600

    Add support for os_region_name
    For environments with multiple endpoints due to regions, tasks such as
    cinder volume attach will not work unless os_region_name is set. This
    allows the os_region_name to be passed in.

--- spec/classes/nova_init_spec.rb
+++ spec/classes/nova_init_spec.rb
@@ -133,7 +133,8 @@ describe 'nova' do
           :nova_user_id             => '499',
           :nova_group_id            => '499',
           :report_interval          => '60',
-          :nova_shell               => '/bin/bash' }
+          :nova_shell               => '/bin/bash',
+          :os_region_name           => 'MyRegion' }
       end
 
       it 'creates user and group' do
@@ -199,6 +200,7 @@ describe 'nova' do
         should contain_nova_config('DEFAULT/notification_topics').with_value('openstack')
         should contain_nova_config('DEFAULT/notify_api_faults').with_value(true)
         should contain_nova_config('DEFAULT/report_interval').with_value('60')
+        should contain_nova_config('DEFAULT/os_region_name').with_value('MyRegion')
       end
 
       context 'with multiple notification_driver' do
